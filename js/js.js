22ab4a9779e30e6a8ca51d63ee5fe4a2a1c5e130
alexgaya <alejandrogaya1994@gmail.com>
2019-02-19 19:39:55 +0100

    added footer, jquery effects and styles

--- js/js.js
+++ js/js.js
@@ -1,4 +1,20 @@
-$('.dropButton').click(function(event){
+//JQuery animations
+
+
+//TOGGLE
+$('.dropButton').click(function (event) {
     var elem = $(event.target).closest('div').find('.dropContent');
     elem.fadeToggle('slow');
 });
+
+//SLIDE
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+   $("#navbar").slideDown(300);
+  } else {
+    $("#navbar").slideUp(300);
+  }
+  prevScrollpos = currentScrollPos;
+}
